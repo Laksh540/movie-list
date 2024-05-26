0b02380947fc77d15c095f80e92c3de6022aeade
Laksh <laksh.prabhudessai@gmail.com>
2024-05-26 10:54:32 +0530

    IconButton Component

---
+++ src/Components/IconButton/IconButton.tsx
@@ -0,0 +1,57 @@
+import React from "react";
+import {
+  BUTTON_VARIANT,
+  TButtonVariant,
+  TIconButtonType,
+} from "../../constants";
+import leftArrow from "../../assets/left-arrow.png";
+import rightArrow from "../../assets/right-arrow.png";
+import { log } from "console";
+
+interface IButtonProps {
+  onClick?: () => void;
+  type: TIconButtonType;
+  imgClassName: string;
+  onMouseDown?: () => void;
+  onMouseUp?: () => void;
+}
+const BUTTON_TYPES = {
+  LEFT_ARROW: {
+    icon: leftArrow,
+    className: " filter-invert-1 ",
+  },
+  RIGHT_ARROW: {
+    icon: rightArrow,
+    className: " filter-invert-1 ",
+  },
+};
+const IconButton = (props: IButtonProps) => {
+  const { onClick, type, imgClassName, onMouseDown, onMouseUp } = props;
+
+  const getImgUrl = () => {
+    return BUTTON_TYPES?.[type]?.icon;
+  };
+
+  const getClassName = () => {
+    let finalClassName =
+      "w-full h-full  cursor-pointer bg-transparent border-none ";
+
+    if (BUTTON_TYPES?.[type]?.className) {
+      finalClassName += BUTTON_TYPES?.[type]?.className;
+    }
+    return finalClassName;
+  };
+
+  return (
+    <button
+      className={getClassName()}
+      onClick={onClick}
+      onMouseDown={onMouseDown}
+      onMouseUp={onMouseUp}
+    >
+      <img src={getImgUrl()} className={imgClassName} />
+    </button>
+  );
+};
+
+export default IconButton;
--- src/constants.ts
+++ src/constants.ts
@@ -3,3 +3,9 @@ export const BUTTON_VARIANT: Record<TButtonVariant, TButtonVariant> = {
   PRIMARY: "PRIMARY",
   SECONDARY: "SECONDARY",
 };
+
+export type TIconButtonType = "LEFT_ARROW" | "RIGHT_ARROW";
+export const ICON_BUTTON_TYPE: Record<TIconButtonType, TIconButtonType> = {
+  LEFT_ARROW: "LEFT_ARROW",
+  RIGHT_ARROW: "RIGHT_ARROW",
+};
